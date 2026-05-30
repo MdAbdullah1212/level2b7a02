40896026689d3fb331e79faf847c3dc8aed46335
MdAbdullah1212 <mda248256@gmail.com>
2026-05-30 18:31:42 +0600

    fix vercel app

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import config from "./config";
 import { initDB } from "./db";
 
 const main = () => {
-  return app.listen(config.port, () => {
+  return app.listen(5000, () => {
     initDB();
-    console.log(`Example app listening on port ${config.port}`);
+    console.log(`Example app listening on port 5000`);
   });
 };
 main()
